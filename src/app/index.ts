46de9cd16902adcad98e210bfcd231263b2f50d5
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-09-20 16:56:53 +0100

    Comment out unused line (but helpful for illustration for now.).

--- src/app/index.ts
+++ src/app/index.ts
@@ -100,8 +100,8 @@ export const layout = {
   },
 };
 
-
-import BluetoothManager = require('./bluetooth-manager');
+//import BluetoothManager = require('./bluetooth-manager');
+require('./bluetooth-manager');
 
 //var manager = BluetoothManager();
 //manager.connect("7C:E9:D3:BB:72:0D")
